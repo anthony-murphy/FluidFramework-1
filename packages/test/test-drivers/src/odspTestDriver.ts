8e5f1b9e08fff4c2ad14a29250af366e52a84639
Tony Murphy <anthonm@microsoft.com>
2021-02-19 14:05:22 -0800

    Move stress tests to test drivers

--- packages/test/test-drivers/src/odspTestDriver.ts
+++ packages/test/test-drivers/src/odspTestDriver.ts
@@ -3,8 +3,6 @@
  * Licensed under the MIT License.
  */
 
-import assert from "assert";
-import fs from "fs";
 import { IRequest } from "@fluidframework/core-interfaces";
 import { IDocumentServiceFactory, IUrlResolver } from "@fluidframework/driver-definitions";
 import {
@@ -19,7 +17,7 @@ import {
     odspTokensCache,
     getMicrosoftConfiguration,
 } from "@fluidframework/tool-utils";
-import { IClientConfig } from "@fluidframework/odsp-doclib-utils";
+import { getDriveId, getLoginPageUrl, getOdspScope, IOdspTokens } from "@fluidframework/odsp-doclib-utils";
 import { ITestDriver } from "@fluidframework/test-driver-definitions";
 import { pkgVersion } from "./packageVersion";
 
@@ -29,72 +27,103 @@ const passwordTokenConfig = (username, password): OdspTokenConfig => ({
     password,
 });
 
-export interface IOdspConfig extends IClientConfig {
+export interface IOdspTestLoginInfo {
     server: string;
-    driveId: string;
     username: string;
-    directory: string;
+    password: string;
+}
+
+const odspTokenManager = new OdspTokenManager(odspTokensCache);
+
+async function getOdspTokens(loginInfo: IOdspTestLoginInfo): Promise<IOdspTokens> {
+    try {
+        // Ensure fresh tokens here so the test runners have them cached
+        const odspTokens = await odspTokenManager.getOdspTokens(
+            loginInfo.server,
+            getMicrosoftConfiguration(),
+            passwordTokenConfig(loginInfo.username, loginInfo.password),
+            undefined /* forceRefresh */,
+            true /* forceReauth */,
+        );
+        await odspTokenManager.getPushTokens(
+            loginInfo.server,
+            getMicrosoftConfiguration(),
+            passwordTokenConfig(loginInfo.username, loginInfo.password),
+            undefined /* forceRefresh */,
+            true /* forceReauth */,
+        );
+        return odspTokens;
+    } catch (ex) {
+        // Log the login page url in case the caller needs to allow consent for this app
+        const loginPageUrl =
+            getLoginPageUrl(
+                loginInfo.server,
+                getMicrosoftConfiguration(),
+                getOdspScope(loginInfo.server),
+                "http://localhost:7000/auth/callback",
+            );
+
+        console.log("You may need to allow consent for this app. Re-run the tool after allowing consent.");
+        console.log(`Go here allow the app: ${loginPageUrl}\n`);
+
+        throw ex;
+    }
 }
 
 export class OdspTestDriver implements ITestDriver {
-    public static createFromEnv() {
-        const config = JSON.parse(fs.readFileSync("./odspConfig.json", "utf-8")) as IOdspConfig;
-        const password = process.env.fluid__odsp__password;
-        assert(password, "Missing password");
-
-        if (process.env.BUILD_BUILD_ID !== undefined) {
-            config.directory = `${process.env.BUILD_BUILD_ID}/${config.directory}`;
-        }
-
-        return new OdspTestDriver(
-            {
-                ...config,
-                ... getMicrosoftConfiguration(),
-            },
-            password,
-        );
+    public static createFromEnv(): OdspTestDriver {
+        throw new Error("not supported");
+    }
+    public static async create(loginInfo: IOdspTestLoginInfo, defaultDirectory: string) {
+        const odspTokens = await getOdspTokens(loginInfo);
+        const driveId =  await getDriveId(loginInfo.server, "", undefined, { accessToken: odspTokens.accessToken });
+        return new OdspTestDriver(loginInfo, driveId, defaultDirectory);
     }
 
     public readonly type = "odsp";
     public readonly version = pkgVersion;
-    private readonly odspTokenManager = new OdspTokenManager(odspTokensCache);
-    constructor(
-        private readonly config: Readonly<IOdspConfig>,
-        private readonly password: string) { }
+
+    private constructor(
+        private readonly loginInfo: IOdspTestLoginInfo,
+        private readonly driveId: string,
+        private readonly defaultDirectory: string,
+        ) { }
+
     createContainerUrl(testId: string): string {
         throw new Error("Method not implemented.");
     }
 
     createDocumentServiceFactory(): IDocumentServiceFactory {
-        return new OdspDocumentServiceFactory(
+        const documentServiceFactory = new OdspDocumentServiceFactory(
             async (options: OdspResourceTokenFetchOptions) => {
-                const tokens = await this.odspTokenManager.getOdspTokens(
-                    this.config.server,
-                    this.config,
-                    passwordTokenConfig(this.config.username, this.password),
+                const tokens = await odspTokenManager.getOdspTokens(
+                    this.loginInfo.server,
+                    getMicrosoftConfiguration(),
+                    passwordTokenConfig(this.loginInfo.username,  this.loginInfo.password),
                     options.refresh,
                 );
                 return tokens.accessToken;
             },
             async (options: OdspResourceTokenFetchOptions) => {
-                const tokens = await this.odspTokenManager.getPushTokens(
-                    this.config.server,
-                    this.config,
-                    passwordTokenConfig(this.config.username, this.password),
+                const tokens = await odspTokenManager.getPushTokens(
+                    this.loginInfo.server,
+                    getMicrosoftConfiguration(),
+                    passwordTokenConfig(this.loginInfo.username,  this.loginInfo.password),
                     options.refresh,
                 );
                 return tokens.accessToken;
             },
         );
+        return documentServiceFactory;
     }
     createUrlResolver(): IUrlResolver {
         return new OdspDriverUrlResolver();
     }
     createCreateNewRequest(testId: string): IRequest {
         return createOdspCreateContainerRequest(
-            `https://${this.config.server}`,
-            this.config.driveId,
-            this.config.directory,
+            `https://${this.loginInfo.server}`,
+            this.driveId,
+            this.defaultDirectory,
             `${testId}.fluid`,
         );
     }
