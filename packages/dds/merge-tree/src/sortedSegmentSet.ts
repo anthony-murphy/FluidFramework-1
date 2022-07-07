bbacf6f8ee8ee82ebfbdf70a74b8b83fee0b802f
Tony Murphy <anthony.murphy@microsoft.com>
2022-07-07 10:58:33 -0700

    fix duplicates in sorted segment set

--- packages/dds/merge-tree/src/sortedSegmentSet.ts
+++ packages/dds/merge-tree/src/sortedSegmentSet.ts
@@ -28,7 +28,7 @@ export class SortedSegmentSet<
     }
 
     public addOrUpdate(newItem: T, update?: (existingItem: T, newItem: T) => T) {
-        const position = this.findOrdinalPosition(this.getOrdinal(newItem));
+        const position = this.findItemPosition(newItem);
         if (position.exists) {
             if (update) {
                 update(this.ordinalSortedItems[position.index], newItem);
@@ -37,9 +37,8 @@ export class SortedSegmentSet<
             this.ordinalSortedItems.splice(position.index, 0, newItem);
         }
     }
-
     public remove(item: T): boolean {
-        const position = this.findOrdinalPosition(this.getOrdinal(item));
+        const position = this.findItemPosition(item);
         if (position.exists) {
             this.ordinalSortedItems.splice(position.index, 1);
             return true;
@@ -48,7 +47,7 @@ export class SortedSegmentSet<
     }
 
     public has(item: T): boolean {
-        const position = this.findOrdinalPosition(this.getOrdinal(item));
+        const position = this.findItemPosition(item);
         return position.exists;
     }
 
@@ -60,25 +59,54 @@ export class SortedSegmentSet<
             ?? maybeObject.ordinal;
     }
 
-    private findOrdinalPosition(ordinal: string, start?: number, end?: number): { exists: boolean; index: number; } {
+    private findItemPosition(
+        item: T,
+        start?: number,
+        end?: number): { exists: boolean; index: number; } {
         if (this.ordinalSortedItems.length === 0) {
             return { exists: false, index: 0 };
         }
         if (start === undefined || end === undefined) {
-            return this.findOrdinalPosition(ordinal, 0, this.ordinalSortedItems.length - 1);
+            return this.findItemPosition(item, 0, this.ordinalSortedItems.length - 1);
         }
         const index = start + Math.floor((end - start) / 2);
-        if (this.getOrdinal(this.ordinalSortedItems[index]) > ordinal) {
+        const itemOrdinal = this.getOrdinal(item);
+        const indexOrdinal = this.getOrdinal(this.ordinalSortedItems[index]);
+        if (indexOrdinal > itemOrdinal) {
             if (start === index) {
                 return { exists: false, index };
             }
-            return this.findOrdinalPosition(ordinal, start, index - 1);
-        } else if (this.getOrdinal(this.ordinalSortedItems[index]) < ordinal) {
+            return this.findItemPosition(item, start, index - 1);
+        }
+
+        if (indexOrdinal < itemOrdinal) {
             if (index === end) {
                 return { exists: false, index: index + 1 };
             }
-            return this.findOrdinalPosition(ordinal, index + 1, end);
+            return this.findItemPosition(item, index + 1, end);
         }
-        return { exists: true, index };
+
+        // at this point we've found the ordinal of the item
+        // so we need to find the index of the item instance
+        //
+        if (item === this.ordinalSortedItems[index]) {
+            return { exists: true, index };
+        }
+
+        let result = { exists: false, index };
+        if (index > start && this.getOrdinal(this.ordinalSortedItems[index - 1]) === itemOrdinal) {
+            result = this.findItemPosition(item, start, index - 1);
+            // only return the back index if the item exist
+            if (result.exists) {
+                return result;
+            }
+        }
+        if (index < end && this.getOrdinal(this.ordinalSortedItems[index + 1]) === itemOrdinal) {
+            // return the forward index regardless of if the item exists
+            // this should be the last index with a matching ordinal
+            result = this.findItemPosition(item, index + 1, end);
+        }
+
+        return result;
     }
 }
