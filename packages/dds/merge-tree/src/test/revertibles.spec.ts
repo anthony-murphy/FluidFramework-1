6437b52ef90dc935faa4816c754344c742e55077
Tony Murphy <anthony.murphy@microsoft.com>
2022-09-02 14:55:35 -0700

    add some smoke tests

--- packages/dds/merge-tree/src/test/revertibles.spec.ts
+++ packages/dds/merge-tree/src/test/revertibles.spec.ts
@@ -8,6 +8,87 @@ import { appendToRevertibles, MergeTreeDeltaRevertible, revert } from "../revert
 import { createClientsAtInitialState, TestClientLogger } from "./testClientLogger";
 
 describe("MergeTree.Revertibles", () => {
+    it("revert insert", () => {
+        const clients = createClientsAtInitialState(
+            { initialState: "123", options: { mergeTreeUseNewLengthCalculations: true } },
+            "A", "B");
+        const logger = new TestClientLogger(clients.all);
+        let seq = 0;
+        const ops: ISequencedDocumentMessage[] = [];
+
+        const clientB_Revertibles: MergeTreeDeltaRevertible[] = [];
+        // the test logger uses these callbacks, so preserve it
+        const old = clients.B.mergeTreeDeltaCallback;
+        clients.B.mergeTreeDeltaCallback = (op, delta) => {
+            old?.(op, delta);
+            appendToRevertibles(clientB_Revertibles, clients.B, delta);
+        };
+        ops.push(clients.B.makeOpMessage(clients.B.insertTextLocal(0, "BB"), ++seq));
+
+        ops.splice(0).forEach((op) => clients.all.forEach((c) => c.applyMsg(op)));
+        logger.validate({ baseText: "BB123" });
+
+        const revertOp = revert(clients.B, ... clientB_Revertibles);
+        revertOp.ops.forEach((op) => ops.push(clients.B.makeOpMessage(op, ++seq)));
+
+        ops.splice(0).forEach((op) => clients.all.forEach((c) => c.applyMsg(op)));
+        logger.validate({ baseText: "123" });
+    });
+
+    it("revert remove", () => {
+        const clients = createClientsAtInitialState(
+            { initialState: "123", options: { mergeTreeUseNewLengthCalculations: true } },
+            "A", "B");
+        const logger = new TestClientLogger(clients.all);
+        let seq = 0;
+        const ops: ISequencedDocumentMessage[] = [];
+
+        const clientB_Revertibles: MergeTreeDeltaRevertible[] = [];
+        // the test logger uses these callbacks, so preserve it
+        const old = clients.B.mergeTreeDeltaCallback;
+        clients.B.mergeTreeDeltaCallback = (op, delta) => {
+            old?.(op, delta);
+            appendToRevertibles(clientB_Revertibles, clients.B, delta);
+        };
+        ops.push(clients.B.makeOpMessage(clients.B.removeRangeLocal(0, 1), ++seq));
+
+        ops.splice(0).forEach((op) => clients.all.forEach((c) => c.applyMsg(op)));
+        logger.validate({ baseText: "23" });
+
+        const revertOp = revert(clients.B, ... clientB_Revertibles);
+        revertOp.ops.forEach((op) => ops.push(clients.B.makeOpMessage(op, ++seq)));
+
+        ops.splice(0).forEach((op) => clients.all.forEach((c) => c.applyMsg(op)));
+        logger.validate({ baseText: "123" });
+    });
+
+    it("revert annotate", () => {
+        const clients = createClientsAtInitialState(
+            { initialState: "123", options: { mergeTreeUseNewLengthCalculations: true } },
+            "A", "B");
+        const logger = new TestClientLogger(clients.all);
+        let seq = 0;
+        const ops: ISequencedDocumentMessage[] = [];
+
+        const clientB_Revertibles: MergeTreeDeltaRevertible[] = [];
+        // the test logger uses these callbacks, so preserve it
+        const old = clients.B.mergeTreeDeltaCallback;
+        clients.B.mergeTreeDeltaCallback = (op, delta) => {
+            old?.(op, delta);
+            appendToRevertibles(clientB_Revertibles, clients.B, delta);
+        };
+        ops.push(clients.B.makeOpMessage(clients.B.annotateRangeLocal(0, 1, { test: 1 }, undefined), ++seq));
+
+        ops.splice(0).forEach((op) => clients.all.forEach((c) => c.applyMsg(op)));
+        logger.validate({ baseText: "123" });
+
+        const revertOp = revert(clients.B, ... clientB_Revertibles);
+        revertOp.ops.forEach((op) => ops.push(clients.B.makeOpMessage(op, ++seq)));
+
+        ops.splice(0).forEach((op) => clients.all.forEach((c) => c.applyMsg(op)));
+        logger.validate({ baseText: "123" });
+    });
+
     it("Remove All Original Text and Insert then Revert", () => {
         const clients = createClientsAtInitialState(
             { initialState: "1-2--", options: { mergeTreeUseNewLengthCalculations: true } },
